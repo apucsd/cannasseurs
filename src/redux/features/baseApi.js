94d71de20946675635c93d0c585862b480d19c2c
apucsd <apusutradhar77@gmail.com>
2025-02-09 11:36:39 +0600

    feat: :sparkles: change the IP and base url

--- src/redux/features/baseApi.js
+++ src/redux/features/baseApi.js
@@ -1,10 +1,11 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-export const baseUrl = 'https://fahim.binarybards.online';
+export const baseUrl = 'http://68.178.202.182:5000';
 const baseApi = createApi({
         reducerPath: 'baseApi',
         baseQuery: fetchBaseQuery({
                 baseUrl: `${baseUrl}/api/v1`,
+                credentials: 'include',
                 prepareHeaders: (headers, { endpoint }) => {
                         // console.log(endpoint);
                         if (endpoint == 'resetPassword') {
